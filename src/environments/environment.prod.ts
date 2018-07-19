2d81fb6c63fcb7574b429d7dd024970c855d6fe1
Oleg Musienko <Diogen737@gmail.com>
2018-07-19 17:08:41 +0300

    production build fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-	production: true
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyDQdzy-pB5OcA57RTM2tY6_1LcZTo0lrss',
+		authDomain: 'ng-playground-diogen737.firebaseapp.com',
+		databaseURL: 'https://ng-playground-diogen737.firebaseio.com',
+		projectId: 'ng-playground-diogen737',
+		storageBucket: 'ng-playground-diogen737.appspot.com',
+		messagingSenderId: '717958923709'
+	}
 };
