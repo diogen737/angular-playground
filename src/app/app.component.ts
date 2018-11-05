ddaa27fd47044819af70a28306726426882e1959
Oleg Musienko <Diogen737@gmail.com>
2018-11-05 22:55:17 +0300

    upgrade packages (Angular 6.0 -> 7.0)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
-import { NotificationsService } from 'angular2-notifications';
+import { NotificationsService, NotificationType } from 'angular2-notifications';
 
 import { NOTIFICATION_OPTIONS } from './shared/notification-options';
 import { AppNotificationService } from './shared/providers/app-notification.service';
 import { NotificationData } from './shared/model/notification-data';
-import { NotificationType } from './shared/model/notification-type';
 
 @Component({
 	selector: 'app-root',
@@ -21,7 +20,7 @@ export class AppComponent implements OnInit {
 
 	ngOnInit() {
 		this.notifier.notificationStream$.subscribe((event: NotificationData) => {
-			this.notificationsService.create(event.title, event.content, NotificationType[event.type].toLowerCase());
+			this.notificationsService.create(event.title, event.content, event.type);
 		});
 	}
 }
