68e7f312925664e9104eace8597191e040ae6eb0
Oleg Musienko <Diogen737@gmail.com>
2018-11-19 17:53:24 +0100

    display login link only if user is not signed in

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from './providers/auth.service';
 
 @Component({
 	selector: 'app-auth',
 	templateUrl: './auth.component.html',
 	styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {}
+export class AuthComponent {
+
+	signedIn: boolean;
+
+	constructor(private authService: AuthService) {
+		authService.isLoggedIn()
+			.then(signedIn => this.signedIn = signedIn);
+	}
+}
