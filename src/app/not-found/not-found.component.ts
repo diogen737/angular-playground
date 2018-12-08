3dcc1dba96a1b45eaf1f3c0b1e998faa49969a84
Oleg Musienko <Diogen737@gmail.com>
2018-12-08 17:47:34 +0100

    404 page style refactoring

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
 	selector: 'app-not-found',
 	templateUrl: './not-found.component.html',
-	styleUrls: ['./not-found.component.scss'],
+	styleUrls: ['./not-found.component.scss', '../shared/styles/landing-bg.scss'],
 	encapsulation: ViewEncapsulation.None
 })
 export class NotFoundComponent {}
