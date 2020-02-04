9fda8a217c592035d539ecfdadc13e9893359462
Oleg Musienko <Diogen737@gmail.com>
2020-02-05 00:24:18 +0300

    [master] Setup new firebase app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
 	production: true,
 	firebase: {
-		apiKey: 'AIzaSyDQdzy-pB5OcA57RTM2tY6_1LcZTo0lrss',
-		authDomain: 'ng-playground-diogen737.firebaseapp.com',
-		databaseURL: 'https://ng-playground-diogen737.firebaseio.com',
-		projectId: 'ng-playground-diogen737',
-		storageBucket: 'ng-playground-diogen737.appspot.com',
-		messagingSenderId: '717958923709'
+		apiKey: 'AIzaSyBgf2xHzpR1Dpg85IljjpyWgPMctnjNkgw',
+		authDomain: 'ng-yard.firebaseapp.com',
+		databaseURL: 'https://ng-yard.firebaseio.com',
+		projectId: 'ng-yard',
+		storageBucket: 'ng-yard.appspot.com',
+		messagingSenderId: '751570145661',
+		appId: '1:751570145661:web:dd30a680ca103a48a67c14',
+		measurementId: 'G-1V62QQL7ML'
 	},
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
 	production: false,
-	firebase: {
-		apiKey: 'AIzaSyDQdzy-pB5OcA57RTM2tY6_1LcZTo0lrss',
-		authDomain: 'ng-playground-diogen737.firebaseapp.com',
-		databaseURL: 'https://ng-playground-diogen737.firebaseio.com',
-		projectId: 'ng-playground-diogen737',
-		storageBucket: 'ng-playground-diogen737.appspot.com',
-		messagingSenderId: '717958923709'
-	},
+	firebase: {},
 };
 
 /*
